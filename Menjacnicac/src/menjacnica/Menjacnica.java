eb8ceb8ef4d9da2c2942f5efc67167878c44e55a
knezevicmarko14316 <knezevicmarko1432016@gmail.com>
2018-03-22 19:13:52 +0100

    Implementiran interfejs na master grani

--- Menjacnicac/src/menjacnica/Menjacnica.java
+++ Menjacnicac/src/menjacnica/Menjacnica.java
@@ -10,19 +10,42 @@ public class Menjacnica implements KursInterfejs {
 	@Override
 	public boolean dodajKurs(Kurs k) {
 		// TODO Auto-generated method stub
-		return false;
+		if (k == null || kursnaLista.contains(k)) {
+			System.out.println("Kurs je null ili je vec unet");
+			return false;
+		}
+			
+		
+		kursnaLista.add(k);
+		return true;
 	}
 
 	@Override
 	public boolean obrisiKurs(Kurs k) {
 		// TODO Auto-generated method stub
-		return false;
+		if (k == null || !kursnaLista.contains(k))
+		{
+			System.out.println("Kurs je null ili ne postoji u listi!");
+			return false;
+		}
+
+		kursnaLista.remove(k);
+		return true;
 	}
 
 	@Override
 	public Kurs pronadjiKurs(String naziv, GregorianCalendar datum) {
 		// TODO Auto-generated method stub
+		if (naziv == null||naziv=="" || datum==null)
+		{
+			System.out.println("Argumenti nisu ispravno uneti!");
+			return null;
+		}
+		for (int i=0;i<kursnaLista.size();i++){
+			if (kursnaLista.get(i).getNazivValute().contains(naziv) &&  kursnaLista.get(i).getDatum()==datum)
+				
+		return kursnaLista.get(i);}
 		return null;
+		
 	}
-
 }
