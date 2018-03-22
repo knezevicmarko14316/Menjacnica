b7b3509bdf1322314fbda7f1010588a1b064357f
knezevicmarko14316 <knezevicmarko1432016@gmail.com>
2018-03-22 19:05:17 +0100

    Implementirane metode interfejsa

--- Menjacnicac/src/menjacnica/Menjacnica.java
+++ Menjacnicac/src/menjacnica/Menjacnica.java
@@ -10,19 +10,34 @@ public class Menjacnica implements KursInterfejs {
 	@Override
 	public boolean dodajKurs(Kurs k) {
 		// TODO Auto-generated method stub
-		return false;
+		if (k == null || kursnaLista.contains(k))
+			throw new RuntimeException("Kurs je null ili vec postoji");
+		
+		kursnaLista.add(k);
+		return true;
 	}
 
 	@Override
 	public boolean obrisiKurs(Kurs k) {
 		// TODO Auto-generated method stub
-		return false;
+		if (k == null || !kursnaLista.contains(k))
+			throw new RuntimeException("Knjiga je null ili ne postoji");
+
+		kursnaLista.remove(k);
+		return true;
 	}
 
 	@Override
 	public Kurs pronadjiKurs(String naziv, GregorianCalendar datum) {
 		// TODO Auto-generated method stub
+		if (naziv == null||naziv=="" || datum==null)
+			throw new RuntimeException("Unesite naziv i datum kursa");
+		for (int i=0;i<kursnaLista.size();i++){
+			if (kursnaLista.get(i).getNazivValute().contains(naziv) &&  kursnaLista.get(i).getDatum()==datum)
+				
+		return kursnaLista.get(i);}
 		return null;
+		
 	}
 
 }
