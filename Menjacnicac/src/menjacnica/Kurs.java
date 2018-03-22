599dda3919c93fee658a45351e4da3db822a3898
knezevicmarko14316 <knezevicmarko1432016@gmail.com>
2018-03-22 18:14:43 +0100

    Dodate metode equals,hash code i toString

--- Menjacnicac/src/menjacnica/Kurs.java
+++ Menjacnicac/src/menjacnica/Kurs.java
@@ -43,6 +43,62 @@ public class Kurs {
 	public void setKupovniKurs(double kupovniKurs) {
 		this.kupovniKurs = kupovniKurs;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((nazivValute == null) ? 0 : nazivValute.hashCode());
+		temp = Double.doubleToLongBits(prodajniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((skracenNazivValute == null) ? 0 : skracenNazivValute.hashCode());
+		temp = Double.doubleToLongBits(srednjiKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovniKurs) != Double.doubleToLongBits(other.kupovniKurs))
+			return false;
+		if (nazivValute == null) {
+			if (other.nazivValute != null)
+				return false;
+		} else if (!nazivValute.equals(other.nazivValute))
+			return false;
+		if (Double.doubleToLongBits(prodajniKurs) != Double.doubleToLongBits(other.prodajniKurs))
+			return false;
+		if (skracenNazivValute == null) {
+			if (other.skracenNazivValute != null)
+				return false;
+		} else if (!skracenNazivValute.equals(other.skracenNazivValute))
+			return false;
+		if (Double.doubleToLongBits(srednjiKurs) != Double.doubleToLongBits(other.srednjiKurs))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Kurs [nazivValute=" + nazivValute + ", skracenNazivValute=" + skracenNazivValute + 
+				", datum=" + datum.get(GregorianCalendar.DAY_OF_MONTH)
+		+"." +datum.get(GregorianCalendar.MONTH)+"."+datum.get(GregorianCalendar.YEAR)
+		+ ", prodajniKurs=" + prodajniKurs + ", srednjiKurs=" + srednjiKurs + ", kupovniKurs=" + kupovniKurs
+				+ "]";
+	}
 
 	
 }
